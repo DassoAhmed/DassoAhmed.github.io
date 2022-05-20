8c8d1838a1cd704681c897089519428cdd3ce83a
DassoAhmed <dassoahmed@gmail.com>
2022-05-21 01:37:59 +0200

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,34 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+//get data from form  
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+// $name = $_POST['name'];
+// $email= $_POST['email'];
+// $message= $_POST['message'];
+// $to = "dassoahmed@mail.com";
+// $subject = "Mail From website";
+// $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+// $headers = "From: noreply@yoursite.com" . "\r\n" .
+// "CC: somebodyelse@example.com";
+// if($email!=NULL){
+//     mail($to,$subject,$txt,$headers);
+// }
+$to = "dassoahmed@gmail.com";
+$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+$from = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$subject = filter_input(INPUT_POST, 'subject', FILTER_SANITIZE_SPECIAL_CHARS);
+$message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_SPECIAL_CHARS);
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+if (filter_var($from, FILTER_VALIDATE_EMAIL)) {
+    $headers = ['From' => ($name?"<$name> ":'').$from,
+            'X-Mailer' => 'PHP/' . phpversion()
+           ];
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+    mail($to, $subject, $message."\r\n\r\nfrom: ".$_SERVER['REMOTE_ADDR'], $headers);
+    die('OK');
+    
+} else {
+    die('Invalid address');
+}
+//redirect
+echo"message send";
 ?>
